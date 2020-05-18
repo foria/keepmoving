a42a15f3e50f2c9ac65d27211871c140d744b229
Marco Melluso <marcomelluso@Marcos-MacBook.local>
2020-05-18 16:44:41 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
 	function start(counter){
-	  if(counter < 14){
+	  if(counter < 12){
 	    setTimeout(function(){
 	    	console.log(counter);
 		    $('.step-'+counter).removeClass('show');
 		    $('.step-'+(counter+1)).addClass('show');
 	      	counter++;
 	      	start(counter);
-	    }, 1000);
+	    }, 150);
 	  } else {
-	    $('.step-14').removeClass('show');
+	    $('.step-12').removeClass('show');
 	    $('.step-1').addClass('show');
 		start(1);
 	  }
