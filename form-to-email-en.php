de7bdf439cac1a2df68d99c84d21a436197155bb
Marco Melluso <marcomelluso@Marcos-MacBook.local>
2020-06-09 12:12:43 +0800

    update

---
+++ form-to-email-en.php
@@ -0,0 +1,55 @@
+<?php 
+	$event = $_POST['event'];
+	$name = $_POST['name'];
+	$lastname = $_POST['lastname'];
+	$groupnumber = $_POST['groupnumber'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	function IsInjected($str)
+	{
+	    $injections = array('(\n+)',
+	           '(\r+)',
+	           '(\t+)',
+	           '(%0A+)',
+	           '(%0D+)',
+	           '(%08+)',
+	           '(%09+)'
+	           );
+	               
+	    $inject = join('|', $injections);
+	    $inject = "/$inject/i";
+	    
+	    if(preg_match($inject,$str))
+	    {
+	      return true;
+	    }
+	    else
+	    {
+	      return false;
+	    }
+	}
+
+	if(IsInjected($email))
+	{
+	    echo "Bad email value!";
+	    exit;
+	}
+
+	$to = "foriaa@gmail.com";
+	//$to = "info@keepmovingbcn.com";
+
+	$email_subject = "New Form Contact";
+
+	$email_body = "New reservation request:\n EVENT:$event.\n NAME:$name.\n LAST-NAME:$lastname.\n GROUP SIZE:$groupnumber.\n PHONE:$phone.\n EMAIL:$email.\n MESSAGE:\n $message".
+
+	$headers = "From: noreply@keepmovingbcn.com \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	echo "Thank you, in brief you will receive a confirmation mail and the indications to complete your reservation.";
+	sleep(3);
+	header("Location: http://keepmovingbcn.com/"); 
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,19 +1,63 @@
 $(document).ready(function(){
 
-	function start(counter){
+	function manWalking(counter){
 	  if(counter < 12){
 	    setTimeout(function(){
-	    	console.log(counter);
+	    	//console.log(counter);
 		    $('.step-'+counter).removeClass('show');
 		    $('.step-'+(counter+1)).addClass('show');
 	      	counter++;
-	      	start(counter);
+	      	manWalking(counter);
 	    }, 150);
 	  } else {
 	    $('.step-12').removeClass('show');
 	    $('.step-1').addClass('show');
-		start(1);
+		manWalking(1);
 	  }
 	}
-	start(1);
+	manWalking(1);
+
+	function dj(){
+	   setTimeout(function(){
+		    $('.service-dj').addClass('animation');
+	    }, 1500);
+	   setTimeout(function(){
+		    $('.service-dj').removeClass('animation');
+			dj();
+	    }, 2000);
+	}
+	dj();
+
+	function mix(){
+	   setTimeout(function(){
+		    $('.service-mix').addClass('animation');
+	    }, 1000);
+	   setTimeout(function(){
+		    $('.service-mix').removeClass('animation');
+			mix();
+	    }, 2000);
+	}
+	mix();
+
+	function events(){
+	   setTimeout(function(){
+		    $('.service-events').addClass('animation');
+	    }, 1500);
+	   setTimeout(function(){
+		    $('.service-events').removeClass('animation');
+			events();
+	    }, 2000);
+	}
+	events();
+
+	function comm(){
+	   setTimeout(function(){
+		    $('.service-comm').addClass('animation');
+	    }, 1000);
+	   setTimeout(function(){
+		    $('.service-comm').removeClass('animation');
+			comm();
+	    }, 1500);
+	}
+	comm();
 })
